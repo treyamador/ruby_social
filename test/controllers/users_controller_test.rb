2ffcf72bca831a6bdde7ebc76862753f666f04cd
Trey Amador <tmamador@cpp.edu>
2019-02-10 06:45:31 -0800

    signup page added

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get signup_path
+    # assert_select "title", "Sign up | #{@base_title}"
     assert_response :success
   end
 
