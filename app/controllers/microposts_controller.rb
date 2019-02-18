a8df53bf8f089cbf17df5d72b6be4f1cbab1b05a
Trey Amador <tmamador@cpp.edu>
2019-02-18 01:23:15 -0800

    image upload with CarrierWave and image resizing with ImageMagick

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -20,7 +20,7 @@ class MicropostsController < ApplicationController
   end
 
   def micropost_params
-    params.require(:micropost).permit(:content)
+    params.require(:micropost).permit(:content, :picture)
   end
 
   def correct_user
